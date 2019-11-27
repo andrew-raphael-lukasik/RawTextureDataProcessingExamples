74461b1d02fc4d105243aca68e910378229f0049
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2019-11-28 00:36:35 +0100

    RawTextureDataProcessingExamplesWindow ported to UIElements

--- Editor/RawTextureDataProcessingExamplesWindow.cs
+++ Editor/RawTextureDataProcessingExamplesWindow.cs
@@ -1,71 +1,134 @@
 ﻿using UnityEngine;
-using UnityEditor;
-
-using Unity.Mathematics;
-using Unity.Collections;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 using Unity.Jobs;
 
-
-public class RawTextureDataProcessingExamplesWindow : EditorWindow
+public class RawTextureDataProcessingExamplesWindow : UnityEditor.EditorWindow
 {
 
-    Texture2D texture;
+    Texture2D _texture;
     
-    void OnGUI ()
+    void OnEnable ()
     {
-        texture = (Texture2D)EditorGUILayout.ObjectField( texture , typeof(Texture2D) , false );
-        EditorGUILayout.HelpBox("Only uncompressed file formats will work. No DXT1/5,ETC etc. This is meant to demonstate runtime processing not offline one.",MessageType.Info);
-        if( texture!=null )
-        {
-            if( texture.isReadable )
-            {
-                if( GUILayout.Button($"Invert Colors") )
-                {
-                    if( texture.format==TextureFormat.RGB24 )
-                    {
-                        var rawdata = texture.GetRawTextureData<RGB24>();
-                        new InvertRGB24Job { data = texture.GetRawTextureData<RGB24>() }
-                            .Run( rawdata.Length );
-                    }
-                    else if( texture.format==TextureFormat.RGBA32 )
-                    {
-                        var rawdata = texture.GetRawTextureData<RGBA32>();
-                        new InvertRGBA32Job { data = texture.GetRawTextureData<RGBA32>() }
-                            .Run( rawdata.Length );
-                    }
-                    else if( texture.format==TextureFormat.R8 || texture.format==TextureFormat.Alpha8 )
-                    {
-                        var rawdata = texture.GetRawTextureData<byte>();
-                        new InvertByteJob { data = texture.GetRawTextureData<byte>() }
-                            .Run( rawdata.Length );
-                    }
-                    else if( texture.format==TextureFormat.R16 )
-                    {
-                        var rawdata = texture.GetRawTextureData<ushort>();
-                        new InvertR16Job { data = texture.GetRawTextureData<ushort>() }
-                            .Run( rawdata.Length );
-                    }
-                    else if( texture.format==TextureFormat.RGB565 )
-                    {
-                        var rawdata = texture.GetRawTextureData<RGB565>();
-                        new InvertRGB565Job { data = texture.GetRawTextureData<RGB565>() }
-                            .Run( rawdata.Length );
-                    }
-                    else
-                    {
-                        throw new System.NotImplementedException($"{texture.format} processing not implemented");
-                    }
-                    texture.Apply();
-                }
-            }
-            else
-            {
-                EditorGUILayout.HelpBox( "This texture is not readable. Choose different texture or enable \"Read/Write Enabled\" for needs of this demonstration." , MessageType.Error );
-            }
-        }
+		rootVisualElement.Clear();
+
+		var PREVIEW = new Image();
+		{
+			PREVIEW.image = _texture;
+			PREVIEW.scaleMode = ScaleMode.ScaleAndCrop;
+		}
+
+		var BUTTON_INVERT = new Button( ()=> InvertColors( _texture ) );
+		{
+			BUTTON_INVERT.SetEnabled( _texture!=null );
+			BUTTON_INVERT.text = "Invert Colors";
+		}
+
+		var BUTTON_EDGES = new Button( ()=> EdgeDetect( _texture ) );
+		{
+			BUTTON_EDGES.SetEnabled( _texture!=null );
+			BUTTON_EDGES.text = "Edge Detect";
+		}
+		
+		var FIELD = new ObjectField();
+		{
+			FIELD.objectType = typeof(Texture2D);
+			FIELD.value = _texture;
+			FIELD.RegisterValueChangedCallback(
+				(e) =>
+				{
+					var newTexture = e.newValue as Texture2D;
+					if( newTexture!=null )
+					{
+						if( !newTexture.isReadable )
+						{
+							UnityEditor.EditorUtility.DisplayDialog(
+								"Texture is not readable" ,
+								$"Texture '{newTexture.name}' is not readable. Choose different texture or enable \"Read/Write Enabled\" for needs of this demonstration." ,
+								"OK"
+							);
+							return;
+						}
+						_texture = newTexture;
+						PREVIEW.image = newTexture;
+						PREVIEW.SetEnabled( true );
+						BUTTON_INVERT.SetEnabled( true );
+						BUTTON_EDGES.SetEnabled( true );
+					}
+					else
+					{
+						_texture = null;
+						PREVIEW.image = null;
+						PREVIEW.SetEnabled( false );
+						BUTTON_INVERT.SetEnabled( false );
+						BUTTON_EDGES.SetEnabled( false );
+					}
+				}
+			);
+		}
+
+		// add elemenets to root:
+		rootVisualElement.Add( FIELD );
+		rootVisualElement.Add( PREVIEW );
+		rootVisualElement.Add( BUTTON_INVERT );
+		rootVisualElement.Add( BUTTON_EDGES );
     }
 
-    [MenuItem("Window/Raw Texture Data Processing Example")]
-    static void CreateWindow () => EditorWindow.GetWindow<RawTextureDataProcessingExamplesWindow>( nameof(RawTextureDataProcessingExamplesWindow) ).Show();
+	static void InvertColors ( Texture2D tex )
+	{
+		if( tex.format==TextureFormat.RGB24 )
+		{
+			var rawdata = tex.GetRawTextureData<RGB24>();
+			new InvertRGB24Job { data = tex.GetRawTextureData<RGB24>() }
+				.Run( rawdata.Length );
+		}
+		else if( tex.format==TextureFormat.RGBA32 )
+		{
+			var rawdata = tex.GetRawTextureData<RGBA32>();
+			new InvertRGBA32Job { data = tex.GetRawTextureData<RGBA32>() }
+				.Run( rawdata.Length );
+		}
+		else if( tex.format==TextureFormat.R8 || tex.format==TextureFormat.Alpha8 )
+		{
+			var rawdata = tex.GetRawTextureData<byte>();
+			new InvertByteJob { data = tex.GetRawTextureData<byte>() }
+				.Run( rawdata.Length );
+		}
+		else if( tex.format==TextureFormat.R16 )
+		{
+			var rawdata = tex.GetRawTextureData<ushort>();
+			new InvertR16Job { data = tex.GetRawTextureData<ushort>() }
+				.Run( rawdata.Length );
+		}
+		else if( tex.format==TextureFormat.RGB565 )
+		{
+			var rawdata = tex.GetRawTextureData<RGB565>();
+			new InvertRGB565Job { data = tex.GetRawTextureData<RGB565>() }
+				.Run( rawdata.Length );
+		}
+		else
+		{
+			throw new System.NotImplementedException($"{tex.format} processing not implemented");
+		}
+		tex.Apply();
+	}
+
+	static void EdgeDetect ( Texture2D tex )
+	{
+		if( tex.format==TextureFormat.RGB24 )
+		{
+			var rawdata = tex.GetRawTextureData<RGB24>();
+			new EdgeDetectRGB24Job( tex.GetRawTextureData<RGB24>() )
+				.Run( rawdata.Length );
+		}
+		else
+		{
+			throw new System.NotImplementedException($"{tex.format} processing not implemented");
+		}
+		tex.Apply();
+	}
+
+    [UnityEditor.MenuItem("Test/Raw Texture Data/Processing Example")]
+    static void CreateWindow () => UnityEditor.EditorWindow.GetWindow<RawTextureDataProcessingExamplesWindow>( nameof(RawTextureDataProcessingExamplesWindow) ).Show();
 
 }
