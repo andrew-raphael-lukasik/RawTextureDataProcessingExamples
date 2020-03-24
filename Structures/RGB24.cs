85222e0731182f35657470a5a33596c39ce1fc45
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2020-03-24 13:52:31 +0100

    explicit cast operators for RGB24 and RGBA24 structs

--- Structures/RGB24.cs
+++ Structures/RGB24.cs
@@ -1,7 +1,14 @@
+using Unity.Mathematics;
+
 public struct RGB24
 {
+	
 	public byte R, G, B;
 	
-	public static explicit operator Unity.Mathematics.int3 ( RGB24 val ) => new Unity.Mathematics.int3{ x=val.R , y=val.G , z=val.B };
+	public static explicit operator int3 ( RGB24 val ) => new int3{ x=val.R , y=val.G , z=val.B };
+	public static explicit operator RGB24 ( int3 val ) => new RGB24{ R=(byte)val.x , G=(byte)val.y , B=(byte)val.z };
+
+	public static RGB24 operator + ( RGB24 lhs , RGB24 rhs ) => (RGB24)( (int3)lhs + (int3)rhs );
+	public static RGB24 operator - ( RGB24 lhs , RGB24 rhs ) => (RGB24)( (int3)lhs - (int3)rhs );
 
 }
--- Structures/RGBA32.cs
+++ Structures/RGBA32.cs
@@ -1,8 +1,14 @@
+using Unity.Mathematics;
+
 public struct RGBA32
 {
 
 	public byte R, G, B, A;
 
-	public static explicit operator Unity.Mathematics.int4 ( RGBA32 val ) => new Unity.Mathematics.int4{ x=val.R , y=val.G , z=val.B , w=val.A };
-
+	public static explicit operator int4 ( RGBA32 val ) => new int4{ x=val.R , y=val.G , z=val.B , w=val.A };
+	public static explicit operator RGBA32 ( int4 val ) => new RGBA32{ R=(byte)val.x , G=(byte)val.y , B=(byte)val.z , A=(byte)val.w };
+	
+	public static RGBA32 operator + ( RGBA32 lhs , RGBA32 rhs ) => (RGBA32)( (int4)lhs + (int4)rhs );
+	public static RGBA32 operator - ( RGBA32 lhs , RGBA32 rhs ) => (RGBA32)( (int4)lhs - (int4)rhs );
+	
 }
